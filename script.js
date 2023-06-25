7914ce0a8a2c889d64f313cc1c15e2c86fb99194
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-06-25 16:01:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your code here
+    // Set ARIA roles using JavaScript
+    const heading = document.getElementById('portfolio-heading');
+    heading.setAttribute('role', 'heading');
+    heading.setAttribute('aria-level', '1');
+
+    const navigation = document.getElementById('portfolio-navigation');
+    navigation.setAttribute('role', 'navigation');
+
+    const aboutSection = document.getElementById('about');
+    const aboutHeading = aboutSection.querySelector('h2');
+    aboutHeading.setAttribute('role', 'heading');
+    aboutHeading.setAttribute('aria-level', '2');
+
+    const profileImage = aboutSection.querySelector('img');
+    profileImage.setAttribute('role', 'img');
+
+    const projectsSection = document.getElementById('projects');
+    const projectsHeading = projectsSection.querySelector('h2');
+    projectsHeading.setAttribute('role', 'heading');
+    projectsHeading.setAttribute('aria-level', '2');
+
+    const projectList = projectsSection.querySelector('ul');
+    projectList.setAttribute('role', 'list');
+
+    const projectItems = projectList.querySelectorAll('li');
+    projectItems.forEach(item => item.setAttribute('role', 'listitem'));
+  

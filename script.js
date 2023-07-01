98cf5f8fcadf9682ed5215a32a875ba5a887f733
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-07-01 19:15:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// Example JavaScript code for portfolio
 
-// Function to toggle a mobile navigation menu
+
+
 function toggleMenu() {
   var nav = document.getElementById("nav");
   nav.classList.toggle("show");
 }
 
-// Add event listener to the navigation menu button
 var menuButton = document.getElementById("menu-button");
 menuButton.addEventListener("click", toggleMenu);
 
